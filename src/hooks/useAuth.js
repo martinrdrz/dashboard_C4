2d793fe8ecde0ca00fd13c4277e599e20ac5625a
martinrdrz <martinrdrz@hotmail.com>
2025-02-24 08:29:04 -0300

    reemplazado store y hook - ahora falta actualziar su uso enel resto de la aplicacion

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,5 @@
 import { useAuthStore } from './store';
+import authApi from '../api/authApi';
 
 export const useAuth = () => {
     const status = useAuthStore((state) => state.status);
@@ -9,10 +10,53 @@ export const useAuth = () => {
     const onLogout = useAuthStore((state) => state.onLogout);
     //const clearErrorMesage = useAuthStore((state) => state.clearErrorMesage);
 
+    const startLogin = async ({ email, password }) => {
+        checking();
+        try {
+            //const { data } = await calendarApi.post('/auth', { email, password });
+            // const { data } = {
+            //     data: { name: 'martin', uid: 'uidailkmalksd83290834a', token: 'tokenaskdalksjdajsdl983749823eklsd' },
+            // };
+            const { data } = await authApi.post('/auth', { email, password });
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            onLogin({
+                uid: data.uid,
+                name: data.name,
+                email: data.email,
+                photoURL: data.photoURL ?? null, //devuelve data.photoURL si el mismo existe, caso contrario devuelve null
+            });
+        } catch (error) {
+            onLogout('Credenciales incorrectas');
+        }
+    };
+
+    const startLogout = () => {
+        localStorage.clear();
+        onLogout();
+    };
+
+    const checkAuthToken = async () => {
+        const token = localStorage.getItem('token');
+        if (!token) return onLogout();
+        try {
+            const { data } = await authApi.get('/auth/renew');
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            onLogin({ uid: data.uid, name: data.name, email: data.email });
+        } catch (error) {
+            localStorage.clear();
+            onLogout();
+        }
+    };
+
     return {
-        isAuthenticated,
-        user,
-        login,
-        logout,
+        //Propiedades
+        status,
+        errorMessage,
+        //Metodos
+        startLogin,
+        checkAuthToken,
+        startLogout,
     };
 };
